d1447fc0e14dae8a9b34778ca1639f703b56b998
Menezesvm <vinicius.menezes2@ufrpe.br>
2026-09-04 14:34:17 -0200

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+
+		Seller seller = new Seller(20, "Flávia", "flimamenezes@gmail.com", new Date(), 4500.0, obj);
+
+		System.out.println(seller);
 	}
 }
---
+++ src/model/entities/Seller.java
@@ -0,0 +1,110 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birhtDate;
+	private Double BaseSalary;
+
+	private Department department;
+
+	public Seller() {
+	}
+
+	public Seller(Integer id, String name, String email, Date birhtDate,
+				  Double baseSalary, Department department) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birhtDate = birhtDate;
+		BaseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirhtDate() {
+		return birhtDate;
+	}
+
+	public void setBirhtDate(Date birhtDate) {
+		this.birhtDate = birhtDate;
+	}
+
+	public Double getBaseSalary() {
+		return BaseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		BaseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Seller seller = (Seller) o;
+		return Objects.equals(id, seller.id) && Objects.equals(name, seller.name) && Objects.equals(email, seller.email) && Objects.equals(birhtDate, seller.birhtDate) && Objects.equals(BaseSalary, seller.BaseSalary) && Objects.equals(department, seller.department);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = Objects.hashCode(id);
+		result = 31 * result + Objects.hashCode(name);
+		result = 31 * result + Objects.hashCode(email);
+		result = 31 * result + Objects.hashCode(birhtDate);
+		result = 31 * result + Objects.hashCode(BaseSalary);
+		result = 31 * result + Objects.hashCode(department);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "Seller{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", email='" + email + '\'' +
+				", birhtDate=" + birhtDate +
+				", BaseSalary=" + BaseSalary +
+				", department=" + department +
+				'}';
+	}
+}
